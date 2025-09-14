867d783a465a79832bfb362b699fb79866a5cf41
Rashmin <rashmin9c@gmail.com>
2025-09-14 11:00:05 +0530

    added prog b, test passed

--- tests/program-a.ts
+++ tests/program-a.ts
@@ -1,12 +1,15 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { ProgramA } from "../target/types/program_a";
+import { ProgramB } from "../target/types/program_b";
 
 describe("program-a", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.programA as Program<ProgramA>;
+  const programA = anchor.workspace.programA as Program<ProgramA>;
+  const programB = anchor.workspace.programB as Program<ProgramB>;
+
 
   let signer = anchor.web3.Keypair.generate();
 
@@ -15,21 +18,22 @@ describe("program-a", () => {
 
     let [pda_address, bump] = anchor.web3.PublicKey.findProgramAddressSync(
       [Buffer.from("pda"), signer.publicKey.toBuffer()],
-      program.programId
+      programA.programId
     );
 
   console.log("Signer pubkey:", signer.publicKey.toBase58());
   console.log("PDA pubkey:", pda_address.toBase58());
-  console.log("Program ID:", program.programId.toBase58());
+  console.log("Program ID:", programA.programId.toBase58());
   console.log("System Program:", anchor.web3.SystemProgram.programId.toBase58());
     // Add your test here.
-  await airdrop(program.provider.connection, signer.publicKey, 500_000_000_000); // give signer SOL
-  await airdrop(program.provider.connection, pda_address, 500_000_000_000);
+  await airdrop(programA.provider.connection, signer.publicKey, 500_000_000_000); // give signer SOL
+  await airdrop(programA.provider.connection, pda_address, 500_000_000_000);
 
-    const tx = await program.methods.initialize().accounts({
+    const tx = await programA.methods.initialize().accounts({
       pdaAccount: pda_address,
       signer: signer.publicKey,
       systemProgram: anchor.web3.SystemProgram.programId,
+      programB: programB.programId,
     }).signers([signer]).rpc();
 
     console.log("Your transaction signature", tx);
